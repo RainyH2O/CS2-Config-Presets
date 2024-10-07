23b5e0c4659fe2677187cf8f8eb7ed4245e7a467
RainyH2O <RainyH2O@users.noreply.github.com>
2024-10-08 02:20:24 +0800

    chore: :hammer: add copy scripts

---
+++ cp_cfg.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if not defined CS2_HOME (
+    echo Error: env var CS2_HOME is not set.
+    exit /b 1
+)
+
+pushd "%~dp0" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: failed to change directory.
+    exit /b 1
+)
+copy .\*.cfg "%CS2_HOME%\cfg\" /Y >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: failed to copy cfg files.
+    exit /b 1
+)
+copy custom\*.cfg "%CS2_HOME%\cfg\" /Y >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: failed to copy custom cfg files.
+    exit /b 1
+)
+
+endlocal
+exit /b 0
